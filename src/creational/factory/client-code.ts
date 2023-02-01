0a4506f2ce854b2b93cbb417220b6d66703f1579
JoasGomes <jojoca.vitor777@gmail.com>
2023-01-31 23:54:15 -0300

    Factory 01

---
+++ src/creational/factory/client-code.ts
@@ -0,0 +1,9 @@
+import { Car } from './vehicle/car';
+
+const fusca = new Car('Fusca');
+fusca.pickUp('Joana');
+fusca.stop();
+
+const celta = new Car('celta');
+celta.pickUp('Joana');
+celta.stop();
