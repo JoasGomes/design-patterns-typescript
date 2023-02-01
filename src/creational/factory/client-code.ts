902d05a150bafeef331ca8f12390af5182ad83af
JoasGomes <jojoca.vitor777@gmail.com>
2023-02-01 00:30:33 -0300

    Padrão Factory

--- src/creational/factory/client-code.ts
+++ src/creational/factory/client-code.ts
@@ -1,9 +1,29 @@
-import { Car } from './vehicle/car';
+import { CarFactory } from './factories/car-factory';
+import { randomCarAlgorithm } from './main/random-vehicle-algorithm';
+import { randomNumbers } from './utils/random-numbers';
 
-const fusca = new Car('Fusca');
+/* const fusca = new Car('Fusca');
 fusca.pickUp('Joana');
 fusca.stop();
 
 const celta = new Car('celta');
 celta.pickUp('Joana');
-celta.stop();
+celta.stop(); */
+
+/* const carFactory = new CarFactory();
+const fusca = carFactory.getVehicle('Fusca');
+fusca.pickUp('Joana');
+fusca.stop(); */
+
+const carFactory = new CarFactory();
+const customerNames = ['ana', 'joas', 'pedro', 'joao'];
+
+for (let i = 0; i < 10; i++) {
+  const vehicle = randomCarAlgorithm();
+  const name = customerNames[randomNumbers(customerNames.length)];
+  vehicle.pickUp(name);
+  vehicle.stop();
+  const newCar = carFactory.pickUp(name, `novo carro ${randomNumbers(100)}`);
+  newCar.stop();
+  console.log('---');
+}
